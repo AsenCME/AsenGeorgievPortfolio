4d656617b3aeb541fdc341f046bcc41f709a0c9e
AsenCME <asen.connor@abv.bg>
2018-06-28 18:05:37 +0300

    projects done + contacts started

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,9 +1,41 @@
 <template>
-    <div class="contacts"></div>
+    <div class="contacts">
+        <div class="overlay"></div>
+
+        <div class="back-button">
+            <div class="text" @click="transitionBack()">
+                <i class="material-icons">arrow_backwards</i>
+                go back
+            </div>
+            <div class="shape" @click="transitionBack()"></div>
+        </div>
+
+        <!-- Middle Circle Menu -->
+        <div class="fs-wrap">
+            <div class="circle-menu" @click="toggleMenu()">
+                <i class="material-icons">close</i>
+                <div class="circle"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+	methods: {
+		transitionBack() {
+			document.querySelector(".back-button").classList.add("trans");
+			setTimeout(() => {
+				this.$router.push({ path: "/hub" });
+			}, 1500);
+		},
+		toggleMenu() {
+			let circle = document.querySelector(".circle-menu");
+			if (circle.classList.contains("opened")) circle.classList.remove("opened");
+			else circle.classList.add("opened");
+		},
+	},
+};
 </script>
 
 <style lang="scss" scoped>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,21 +1,27 @@
 <template>
-	<div class="projects" @click="swapProject()">
+	<div class="projects">
+
+		<!-- Left panel + Numbers -->
 		<div class="left-panel hidden">
 			<div class="numbers">
 				<div class="current">
-					<div class="text">{{currentNumber + 1}}</div>
+					<div class="text" v-text="currentNumber + 1"></div>
 				</div>
 				<div class="divider">
 				</div>
 				<div class="total">
-					<div>{{totalNumber}}</div>
+					<div v-text="totalNumber"></div>
 				</div>
 			</div>
 		</div>
+
+		<!-- Title/Alias -->
 		<div class="title">
-			<div class="text hidden">Clothing Store in Blagoevgrad</div>
+			<div class="text hidden" v-text="currentProject.title"></div>
 			<div class="line hidden"></div>
 		</div>
+
+		<!-- View Project Button -->
 		<div class="button-wrap">
 			<div class="left-line hidden"></div>
 			<div class="button concealed">
@@ -26,28 +32,48 @@
 			<div class="right-line hidden"></div>
 
 		</div>
+
+		<!-- Main Text + Desc + Line -->
 		<div class="main-text">
 			<div class="cname">
-				<div class="text hidden">Yuitex EOOD</div>
+				<div class="text hidden" v-text="currentProject.name"></div>
 			</div>
 
 			<div class="description">
 				<div class="line hidden"></div>
-				<div class="text hidden">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.</div>
+				<div class="text hidden" v-text="currentProject.description"></div>
 			</div>
 		</div>
 
+		<!-- Mockups (images) -->
 		<div class="mockup hidden">
-			<img src="./../assets/mockups/Yuitex_L.png">
-			<img class="secondary" src="./../assets/mockups/Yuitex_S.png">
-		</div>
-		<div class="back-button"></div>
+			<img :class="{invis: currentNumber !== 0}" src="./../assets/mockups/Yuitex_L.png">
+			<img :class="{invis: currentNumber !== 0}" class="yuitexs" src="./../assets/mockups/Yuitex_S.png">
 
+			<img :class="{invis: currentNumber !== 1}" class="hotelup" src="./../assets/mockups/HU_L.png">
+
+			<img :class="{invis: currentNumber !== 2}" class="itschool" src="./../assets/mockups/ITSchool.png">
+
+			<img :class="{invis: currentNumber !== 3}" class="mesten" src="./../assets/mockups/Mesten_D.png">
+
+			<img :class="{invis: currentNumber !== 4}" class="kosmos" src="./../assets/mockups/Kosmos_L.png">
+		</div>
+
+		<!-- Button to go Back -->
+		<div class="back-button-wrap">
+			<div class="back-button hidden" @click="transitionBack()"></div>
+			<div class="back-arrow" @click="transitionBack()">
+				<i class="material-icons" @click="transitionBack()">arrow_forward</i>
+			</div>
+		</div>
+
+		<!-- Background Text -->
 		<div class="fancy-text">
 			<div v-for="n in rows" :key="n" class="row hidden">
-				<span v-for="k in cols" :key="k">yuitex eood</span>
+				<span v-for="k in cols" :key="k" v-text="currentProject.name"></span>
 			</div>
 		</div>
+
 	</div>
 </template>
 
@@ -60,11 +86,50 @@ export default {
 			cols: 4,
 			currentNumber: 0,
 			totalNumber: 4,
-			currentProject: {},
-			projects: [{}, {}, {}, {}],
+			projects: [
+				{
+					name: "Yuitex EOOD",
+					title: "Clothing Store in Blagoevgrad",
+					description: "Yuitex EOOD is a project.",
+				},
+				{
+					name: "HotelUp",
+					title: "The first Bulgarian hotel App",
+					description: "HotelUp is a project",
+				},
+				{
+					name: "IT'School",
+					title: "It's cool to learn from ITSchool",
+					description: "IT'School is a project",
+				},
+				{
+					name: "Mesten Inc.",
+					title: "Find Local Doctors Quickly",
+					description: "Mesten Inc. is a project",
+				},
+				{
+					name: "Kosmos",
+					title: "Explore the Solar System",
+					description: "Kosmos is a project",
+				},
+			],
 		};
 	},
+	computed: {
+		currentProject() {
+			return this.projects[this.currentNumber];
+		},
+	},
 	methods: {
+		// Transition Function
+		transitionBack() {
+			document.querySelector(".back-button-wrap").classList.add("trans");
+			setTimeout(() => {
+				this.$router.push({ path: "/hub" });
+			}, 1500);
+		},
+
+		// Random Displacement for Background Text
 		getRD() {
 			for (let i = 1; i < this.rows + 1; i++) {
 				let rd = Math.floor(Math.random() * 401) - 200;
@@ -72,6 +137,8 @@ export default {
 				document.querySelector(`.row:nth-of-type(${i})`).style.transform = value;
 			}
 		},
+
+		// One time reveals
 		revealPanel() {
 			document.querySelector(".left-panel").classList.remove("hidden");
 		},
@@ -82,6 +149,7 @@ export default {
 			}, 600);
 		},
 
+		// Main Text Controls
 		revealMainText() {
 			document.querySelector(".cname .text").classList.remove("hidden");
 			document.querySelector(".description .text").classList.remove("hidden");
@@ -93,6 +161,7 @@ export default {
 			document.querySelector(".description .line").classList.add("hidden");
 		},
 
+		// View Project Button Controls
 		revealButton() {
 			document.querySelector(".button").classList.remove("concealed");
 			document.querySelector(".left-line").classList.remove("hidden");
@@ -104,6 +173,7 @@ export default {
 			document.querySelector(".right-line").classList.add("hidden");
 		},
 
+		// Title Controls
 		slideTitleUp() {
 			document.querySelector(".title .text").classList.remove("hidden");
 		},
@@ -111,6 +181,7 @@ export default {
 			document.querySelector(".title .text").classList.add("hidden");
 		},
 
+		// Side Number Controls
 		slideCurrentNumberUp() {
 			document.querySelector(".numbers .current .text").classList.remove("hidden");
 		},
@@ -118,6 +189,7 @@ export default {
 			document.querySelector(".numbers .current .text").classList.add("hidden");
 		},
 
+		// Image Controls
 		slideImageLeft() {
 			document.querySelector(".mockup").classList.remove("hidden");
 		},
@@ -125,6 +197,7 @@ export default {
 			document.querySelector(".mockup").classList.add("hidden");
 		},
 
+		// Backgound Text Controls
 		revealFancyText() {
 			this.getRD();
 			let items = document.querySelectorAll(".fancy-text .row");
@@ -139,27 +212,52 @@ export default {
 			}
 		},
 
-		swapProject() {
+		// Back Button Controls
+		revealBackButton() {
+			document.querySelector(".back-button").classList.remove("hidden");
+		},
+		concealBackButton() {
+			document.querySelector(".back-button").classList.add("hidden");
+		},
+
+		// MouseWheel Scroll Handler
+		MouseWheelHandler(e) {
+			e = window.event || e;
+			let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+			this.swapProject(delta);
+		},
+
+		swapProject(direction) {
 			this.concealMainText();
 			this.concealButton();
 			this.slideTitleDown();
 			this.slideCurrentNumberDown();
 			this.slideImageRight();
 			this.concealFancyText();
-
-			// XXX: Do the swap
+			this.concealBackButton();
 
 			setTimeout(() => {
+				this.currentNumber = this.mod(this.currentNumber - direction, this.totalNumber);
+
 				this.revealMainText();
 				this.revealButton();
 				this.slideTitleUp();
 				this.slideCurrentNumberUp();
 				this.slideImageLeft();
 				this.revealFancyText();
-			}, 1500);
+				this.revealBackButton();
+			}, 1200);
+		},
+
+		mod(n, m) {
+			return (n % m + m) % m;
 		},
 	},
 	created() {
+		// Animate Everything In
+		setTimeout(() => {
+			this.revealBackButton();
+		}, 2000);
 		setTimeout(() => {
 			this.slideImageLeft();
 		}, 1800);
@@ -179,7 +277,18 @@ export default {
 			this.revealPanel();
 		}, 500);
 
+		// Set current Project
 		this.totalNumber = this.projects.length;
+		this.currentProject = this.projects[this.currentNumber];
+
+		// Attach MouseWheel Handler
+		window.addEventListener("mousewheel", this.MouseWheelHandler, false);
+		window.addEventListener("DOMMouseWheel", this.MouseWheelHandler, false);
+	},
+	destroyed() {
+		// Detach MouseWheel Handler
+		window.removeEventListener("mousewheel", this.MouseWheelHandler, false);
+		window.removeEventListener("DOMMouseWheel", this.MouseWheelHandler, false);
 	},
 };
 </script>
